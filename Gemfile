ef1fbd8d526a127cd5ce317e08f7e9dda6c2604b
sawestwood <steve.westwood@hexsaw.org.uk>
2016-10-23 21:20:04 +0100

    Basic spec added for ResolverApp

--- Gemfile
+++ Gemfile
@@ -1,2 +1,7 @@
-gem "sinatra"
-gem "thin"
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'thin'
+gem 'rspec'
+gem 'rack'
+gem 'rack-test'   
---
+++ spec/resolver_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../resolver.rb'
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
+
+describe 'Dice Resolver Service' do
+  include Rack::Test::Methods
+
+  def app
+    ResolverApp
+  end
+
+  it "should return BLUE dice for home team and GREEN dice for away team when home and away teams supplied" do
+    get '/resolve?homeTeam=Wolves&awayTeam=WBA'
+    expect(last_response).to be_ok
+
+    json = JSON.parse(last_response.body)
+    expect(json['homeDice']).to eq 'blue'
+    expect(json['awayDice']).to eq 'green'
+  end
+
+  it "should return error when home team missing from request" do
+    get '/resolve?homeTeam=Wolves'
+    expect(last_response).to_not be_ok
+
+    json = JSON.parse(last_response.body)
+    expect(json['error']).to eq 'Missing away team'
+  end
+
+  it "should return error when away team missing from request" do
+    get '/resolve?awayTeam=WBA'
+    expect(last_response).to_not be_ok
+
+    json = JSON.parse(last_response.body)
+    expect(json['error']).to eq 'Missing home team'
+  end
+
+end
